8118da720451c264c5d61e98fd66fd93f2de953c
GramatunovaN <gramatunova.nata@mail.ru>
2023-02-26 13:39:00 +0700

    Example10

---
+++ lection1/example10/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = { 11, 21, 35, 35, 68, 64, 86, 96, 85 };
+
+int n = array.Length;
+int find = 68;
+
+int index = 0;
+
+while ( index < n)
+
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+
